c5b0bf7a30bf6784720f3a1a7c8749844f723b80
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-29 14:54:02 +0530

    Initial commit
    Usage: $ compass init

--- config.rb
+++ config.rb
@@ -1,1 +1,21 @@
 #!/usr/bin/env ruby
+
+
+# PepXpert® - v1.0.0
+# -----------------------------------------
+# From the Desk of Sequømics Corporation.
+#     With ♥ — by Prabhat Kumar.
+# -----------------------------------------
+# @author     : Prabhat Kumar.
+# @copyright  : Sequømics Corporation.
+# @homepage   : http://sequomics.com/.
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.0
+# @build      : Himalaya
+# @date       : 29-Oct-2016
+# @license    : Apache, version 2.0
+# -----------------------------------------
+# Principle   : Standard Module Definition
+# -----------------------------------------
