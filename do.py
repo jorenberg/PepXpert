1ad2aef2612b584dbe0da70361d15e1af777db08
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-30 02:15:00 +0530

    Initial commit
    MIT License to Apache License.

--- do.py
+++ do.py
@@ -3,7 +3,7 @@
 
 # ————————————————————————————————————————————————————————————————————————————
 # Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
-# Licensed under the MIT License (the "License");
+# Licensed under the Apache License (the "License, version 2.0");
 # you may not use this file except in compliance with the License.
 # ————————————————————————————————————————————————————————————————————————————
 #  ,-.
@@ -22,7 +22,7 @@ __authors__       = [
     '"Prabhat Kumar" <prabhat.genome@gmail.com>',
     '"Sequømics Corporation" <admin@sequomics.com>'
     ]
-__license__       = 'MIT License'
+__license__       = 'Apache License'
 __date__          = '29-10-2016'
 __copyright__     = "Prabhat Kumar"
 __webserver__     = "http://biotools.sequomics.com/pepxpert/"
