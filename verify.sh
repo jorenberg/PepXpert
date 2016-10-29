30d8ee03d8447fc3436cecbda7485a5ef071537c
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-29 16:38:59 +0530

    Initial commit
    verify.sh, to --/verify/-- the builds after completion of "Grunt-Task", for "PepXpert®".
    Usage: $ sh verify.sh

---
+++ verify.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+# =========================== I'm Prabhat® ===========================
+#                               PepXpert®
+# Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2006 - 2016, Prabhat Kumar. All rights reserved.
+# ====================================================================
