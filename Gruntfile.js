8abdc73f613e466b9bb430d34fb016a7952022b8
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-30 19:24:02 +0530

    Initial commit
    Primary Task(s) for SEED™ Building System.

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,3 +73,17 @@ module.exports = function(grunt) {
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'), /* reading 'package.json' for sync package(s) updates, mainly. */
+    
+    // ---------------------------------------------------------------------------|
+    // Primary Task(s) for SEED™ Building System ||                               |
+    // -------------------------------------------                                |
+    // 1. Assembly --> HTML - Lint + Minify                                       |
+    // 2. Sass + Compass Build                                                    |
+    // 3. CSS Lint + Minify                                                       |
+    // 4. Data Build + Lint                                                       |
+    // 5. JavaScript Lint + Concatenation + Minify                                |
+    // 6. Cleaning of temporary files or directories                              |
+    // 7. Localhost server + watch                                                |
+    // 8. Development dependencies updates                                        |
+    // 9. Release(s) with version updates                                         |
+    // ---------------------------------------------------------------------------|
