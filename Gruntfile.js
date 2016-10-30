bf951a2c91498099feb20e9c084c9692e33bb4e7
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-30 21:00:34 +0530

    Initial commit
    Defined Task(concat) for grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -240,3 +240,20 @@ module.exports = function(grunt) {
         '!bower_components/**/*.js' // ignores bower_components.
       ]
     },
+    // 5. JavaScript --> 5.2 Concat
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    concat: {
+      dist: {
+        options: {
+          // Replace all 'use strict' statements in the code with a single one at the top.
+          banner: "'use strict';\n",
+          process: function(src, filepath) {
+            return '// Source: ' + filepath + '\n' + src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+          }
+        },
+        files: {
+          // compiled.js for — intermediate work.
+          './app/en/assets/script/index.compiled.js': ['./source/core/script/**/*.js'],
+        }
+      }
+    },
